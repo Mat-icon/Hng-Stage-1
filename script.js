8dc17bf9e2dbaf486fba21a7039e4c70370e6c7b
Matthew Ameh <matthewameh007@gmail.com>
2024-07-01 13:59:22 +0100

    natural height

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const output = document.querySelector("pre");
+const image = document.querySelector("img");
+
+image.addEventListener("load", (event) => {
+  const { naturalWidth, naturalHeight, width, height } = image;
+  output.textContent = `
+Natural size: ${naturalWidth} x ${naturalHeight} pixels
+Displayed size: ${width} x ${height} pixels
+`;
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     const currentTimeUTC = document.getElementById('currentTimeUTC');
     const currentDay = document.getElementById('currentDay');
